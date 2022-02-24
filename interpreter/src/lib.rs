b2b8ea159cb2420121f526fa5d00b4083553b346
dastansam <dastan.samatov.33@gmail.com>
2022-02-24 02:16:11 +0200

    Push initial impl

--- interpreter/src/lib.rs
+++ interpreter/src/lib.rs
@@ -23,3 +23,83 @@ pub enum Instruction {
     Finish,
 }
 
+/// Program result.
+pub enum ProgramResult {
+    Ok,
+    CompileError,
+    RuntimeError,
+}
+
+impl SupertVM {
+    pub fn new() -> SupertVM {
+        SupertVM {
+            instructions: Vec::new(),
+            stack: Vec::new(),
+        }
+    }
+
+    pub(crate) fn interpret(mut self) -> ProgramResult {
+        loop {
+            match self.instructions.pop() {
+                Some(Instruction::LoadVal(val)) => {
+                    self.stack.push(val);
+                },
+                Some(Instruction::WriteVar(var)) => {
+                    let val = self.stack.pop().unwrap();
+                    println!("{} = {}", var, val);
+                },
+                Some(Instruction::ReadVar(var)) => {
+                    let mut input = String::new();
+                    std::io::stdin().read_line(&mut input).unwrap();
+                    let val = input.trim().parse::<i32>().unwrap();
+                    self.stack.push(val);
+                },
+                Some(Instruction::LoopStart) => {
+                    let val = self.stack.pop().unwrap();
+                    if val == 0 {
+                        self.instructions.pop();
+                    }
+                },
+                Some(Instruction::LoopEnd) => {
+                    let val = self.stack.pop().unwrap();
+                    self.instructions.push(Instruction::LoopStart);
+                    self.instructions.push(Instruction::LoadVal(val));
+                },
+                Some(Instruction::Add) => {
+                    let val2 = self.stack.pop().unwrap();
+                    let val1 = self.stack.pop().unwrap();
+                    self.stack.push(val1 + val2);
+                },
+                Some(Instruction::Sub) => {
+                    let val2 = self.stack.pop().unwrap();
+                    let val1 = self.stack.pop().unwrap();
+                    self.stack.push(val1 - val2);
+                },
+                Some(Instruction::Mul) => {
+                    let val2 = self.stack.pop().unwrap();
+                    let val1 = self.stack.pop().unwrap();
+                    self.stack.push(val1 * val2);
+                },
+                Some(Instruction::Div) => {
+                    let val2 = self.stack.pop().unwrap();
+                    let val1 = self.stack.pop().unwrap();
+                    self.stack.push(val1 / val2);
+                },
+                Some(Instruction::Mod) => {
+                    let val2 = self.stack.pop().unwrap();
+                    let val1 = self.stack.pop().unwrap();
+                    self.stack.push(val1 % val2);
+                },
+                Some(Instruction::Finish) => {
+                    return ProgramResult::Ok;
+                },
+                None => {
+                    return ProgramResult::RuntimeError;
+                },
+                _ => {
+                    return ProgramResult::CompileError;
+                },
+            }
+        }
+    }
+}
